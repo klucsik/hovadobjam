9da360cb833c58148f048b405e8fb1a7429c8ceb
KlucsikKP <klucsik.krisztian@gmail.com>
2019-08-20 14:59:00 +0200

    Alap adat felvitele és lekérdezése, működő verzió

--- tests/hullinfo_test.py
+++ tests/hullinfo_test.py
@@ -2,4 +2,23 @@ from app.hullinfo import *
 
 
 def test_create_hullinfo():
-    assert create_hullinfo('test_create_1', 42, 0).hull_id == 42
+    assert create_hullinfo('test_create_1', 43, 0)[0]['hull_id'] == 43
+
+
+def test_create_hullinfo_update():
+    last_version = create_hullinfo('test_create_1', 42, 0)[0]['version']
+    new_version = create_hullinfo('test_create_1', 42, 0)[0]['version']
+    assert last_version +1 == new_version
+
+def test_get_hullinfo_by_hull_id():
+    create_hullinfo('test_for_get_hullinfo', 666)
+    assert get_hullinfo_by_hull_id(666)[0]['name'] == 'test_for_get_hullinfo'
+
+def test_get_hullinfo_by_name():
+    create_hullinfo('test_for_get_hullinfo', 333)
+    assert get_hullinfo_by_name("test_for_get_hullinfo")[0]['hull_id'] == 333
+
+def test_create_hullinfo_hull_id_creation():
+    first_id = create_hullinfo('test_for_hull_id_increment')[0]['hull_id']
+    second_id = create_hullinfo('test_for_hull_id_increment')[0]['hull_id']
+    assert first_id+1 == second_id
