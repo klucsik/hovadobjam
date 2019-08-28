f276e5cb667f1bffa57a06b48c61084f85ff5a53
KlucsikKP <klucsik.krisztian@gmail.com>
2019-08-28 22:02:56 +0200

    login and register, needs some refactor (úgy ahogy van kiloptam a példákból, át se néztem rendesen)

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 import logging
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
 logging.basicConfig(level=logging.DEBUG)
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class HullinfoKeresesForm(FlaskForm):
     hullinfo_alias = StringField('hullinfo_alias', validators=[DataRequired()])
@@ -12,3 +12,28 @@ class HullinfoHozzaadasForm(FlaskForm):
     hullinfo_name = StringField('hullinfo_name', validators=[DataRequired()])
     hullinfo_description = StringField('hullinfo_description', validators=[DataRequired()])
     submit = SubmitField('Beküldés')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
