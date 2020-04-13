39e0e8179a8a0d2b2e3b21e8f6e476cf35ef63fa
klucsik <klucsik.krisztian@gmail.com>
2020-04-13 12:35:10 +0200

    items base

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,38 @@
+class ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+    end
+    
+    def create
+        @item = Item.new(item_params)
+
+        if @item.save
+        redirect_to @item
+        else
+            render 'new'
+        end
+    end
+    
+    def new
+        @item = Item.new
+    end
+
+    def edit
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+        def item_params
+            params.require(:item).permit(:name)
+        end
+end
